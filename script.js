26bda4fcf155df22f50d9f2cbf34603247b14bf7
Otavio Martins de Castro <otaaviomartinsdecastro@gmail.com>
2024-11-21 10:26:53 -0400

    sdasasd

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+  
+    links.forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
+  
